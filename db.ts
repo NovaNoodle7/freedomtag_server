a36163ac8f00e1c433092162e70931f20d3c6d1d
Alex <novanoodle7@gmail.com>
2025-12-08 10:32:56 +0900

    remove database_url

--- db.ts
+++ db.ts
@@ -6,17 +6,17 @@ import { supabase } from './supabase';
 
 neonConfig.webSocketConstructor = ws;
 
-if (!process.env.SUPABASE_URL && !process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL or SUPABASE_URL must be set.");
-}
+// if (!process.env.SUPABASE_URL && !process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL or SUPABASE_URL must be set.");
+// }
 
-export const pool = process.env.DATABASE_URL 
-  ? new Pool({ connectionString: process.env.DATABASE_URL }) 
-  : undefined;
+// export const pool = process.env.DATABASE_URL 
+//   ? new Pool({ connectionString: process.env.DATABASE_URL }) 
+//   : undefined;
 
-export const db = process.env.DATABASE_URL 
-  ? drizzle({ client: pool!, schema }) 
-  : undefined;
+// export const db = process.env.DATABASE_URL 
+//   ? drizzle({ client: pool!, schema }) 
+//   : undefined;
 
 export { supabase };
 
